6387bdec37bdb3ac25acd6e5df43d6628632b2da
Noah Da Silva <noah@noahdasilva.com>
2022-10-03 23:03:35 -0400

    Added files' hero comments.

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ Looking at the difference between a client-server network application using the 
 The RPC system will be written in Python (`*.py` files), while the RMI system will be written in Java (`*.java` files).
 
 ## Resources
-A good starting place to learn the differences between the two is GeeksForGeeks:
-https://www.geeksforgeeks.org/difference-between-rpc-and-rmi/
+A good starting place to learn the differences between the two is [GeeksForGeeks](https://www.geeksforgeeks.org/difference-between-rpc-and-rmi/)'s article on the subject.
--- rsvserver/rsvserver.java
+++ rsvserver/rsvserver.java
@@ -1,3 +1,28 @@
+/**
+ *  PROJECT : RPC and RMI Systems
+ * 
+ *  FILENAME : rsvserver.java
+ * 
+ *  DESCRIPTION :
+ *      Looking at the difference between a client-server network application using the
+ *      Remote Procedure Call (RPC) and the Remote Method Invocation (RMI) systems.
+ * 
+ *  FUNCTIONS :
+ *      rsvserver.main()
+ * 
+ *  NOTES :
+ *      - ..
+ * 
+ *  AUTHOR(S) : Noah Arcand Da Silva    START DATE : 2022.10.03 (YYYY.MM.DD)
+ *
+ *  CHANGES :
+ *      - ...
+ * 
+ *  VERSION     DATE        WHO             DETAILS
+ *  0.0.1a      2022.10.03  Noah            Creation of project.
+ */
+
+
 package rsvserver;
 
 public class rsvserver {
